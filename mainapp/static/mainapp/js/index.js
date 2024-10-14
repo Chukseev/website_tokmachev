9517556be25b3045c24a70d4a54725ea547afc72
MDayZZ <maxdayzzoff@gmail.com>
2024-10-15 02:27:25 +0300

    fix some bugs

--- mainapp/static/mainapp/js/index.js
+++ mainapp/static/mainapp/js/index.js
@@ -1,7 +1,15 @@
 const callButton = document.getElementById("callButton");
 const closeModalButton = document.getElementById("closeModal")
 const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modalContent");
 
+modal.addEventListener("click", () => {
+    modal.hidden = true;
+})
+
+modalContent.addEventListener("click", (event) => {
+    event.stopPropagation();
+})
 
 callButton.addEventListener("click", () => {
     modal.hidden = false;
