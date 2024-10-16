86f5ab10414c68f160f57d45781a0842a4ea337b
chukseev <chukseev.nikita@yandex.ru>
2024-10-16 21:24:40 +0300

    edit modal.html, forms.py

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,13 +1,12 @@
 from django import forms
 
-
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Your Name', max_length=100)
-    email = forms.EmailField(label='Your Email')
-    message = forms.CharField(widget=forms.Textarea, label='Message')
+    name = forms.CharField(label='Имя', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Электронная почта', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), label='Сообщение')
 
     def clean_name(self):
         name = self.cleaned_data.get('name')
         if len(name) < 3:
-            raise forms.ValidationError('Name must have at least 3 characters.')
+            raise forms.ValidationError('Имя должно содержать минимум 3 символа.')
         return name
